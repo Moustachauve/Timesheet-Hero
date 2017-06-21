1774ca53f1fccf662940a438e0753845ea896ca1
Moustachauve <Moustachauve@users.noreply.github.com>
2017-06-21 17:28:49 -0400

    Fix installer

--- generateWindowsInstaller.js
+++ generateWindowsInstaller.js
@@ -5,8 +5,7 @@ resultPromise = electronInstaller.createWindowsInstaller({
     outputDirectory: 'build/timesheet-hero-installer',
     authors: 'Christophe Gagnier',
     exe: 'Timesheet Hero.exe',
-    setupIcon: 'app/icon.ico',
-    iconUrl: 'app/icon.ico'
+    setupIcon: 'app/icon.ico'
   });
 
 resultPromise.then(() => console.log("It worked!"), (e) => console.log(`No dice: ${e.message}`));
