20067c3a72aa46dd7071eee43c47d264e2b0f584
Moustachauve <Moustachauve@users.noreply.github.com>
2017-07-11 18:33:30 -0400

    Format the save file for better human readability

--- app/lib/lockedData.js
+++ app/lib/lockedData.js
@@ -231,7 +231,7 @@ function saveData(date, data, callback) {
     getFilePath(date, function(err, filePath) {
         if(err) { return callback(err); }
 
-        jsonfile.writeFile(filePath, data, function(err) {
+        jsonfile.writeFile(filePath, data, {spaces: 2}, function(err) {
             if(err) { return callback(err); }
             lockedData.emit('dataChange', date, data);
             return callback();
