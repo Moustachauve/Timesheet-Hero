3f2603592c45eaa2e0a600e964cefad1ff7d7ace
Moustachauve <Moustachauve@users.noreply.github.com>
2017-08-07 00:16:05 -0400

    Reopen the app when icon in the dock is clicked (MacOS)

--- app/main.js
+++ app/main.js
@@ -46,9 +46,7 @@ app.on('window-all-closed', function () {
 app.on('activate', function () {
   // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
-  if (!windowManager.isWindowCreated()) {
-    windowManager.createWindow()
-  }
+  windowManager.createWindow()
 })
 
 var trayIcon
