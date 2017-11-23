2a0117455509fd1e34043a024fdf4baf51af0db3
Nicolas Andrade <nicolas.andrade@lm0039.corp.ssense.com>
2017-11-23 11:03:56 -0500

    Adding support to MacOs for hipsters and people who's stuck with a Mac

--- app/lib/macOs/sessionStateDetector.js
+++ app/lib/macOs/sessionStateDetector.js
@@ -1,12 +1,27 @@
 'use strict'
+const quartz = require('osx-quartz') // https://www.npmjs.com/package/osx-quartz
+      , sessionStateDetector = {}
+      
+let previousLockStatus = true;
 
-var sessionStateDetector = {}
 module.exports = sessionStateDetector
 
 sessionStateDetector.startTracking = function (stateChangedCallback) {
-  // stateChangedCallback(bool);
+
+
+  let monitorLockState = () => {
+    let actualLockStatus = quartz.isScreenLocked();
+    if(actualLockStatus != previousLockStatus) {
+      // Upon a change, call the callback
+      previousLockStatus = actualLockStatus;
+      stateChangedCallback(previousLockStatus);
+    }
+  }
+
+  this.intervalId = setInterval(monitorLockState, 2000) // Run every 2 seconds
+  // https://gist.github.com/abhishekjairath/8bfb259c681ef52545b32c88db6336f5
 }
 
 sessionStateDetector.stopTracking = function () {
-
+  clearInterval(this.intervalId)
 }
