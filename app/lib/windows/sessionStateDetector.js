a1a259b41d5f8b456b5a6a6f35ce2129af23000c
Moustachauve <Moustachauve@users.noreply.github.com>
2018-07-27 16:32:00 -0400

    Add remote session detection for ticket #6

--- app/lib/windows/sessionStateDetector.js
+++ app/lib/windows/sessionStateDetector.js
@@ -9,7 +9,8 @@ var unsubscribeCsharpEvent
 
 sessionStateDetector.startTracking = function (stateChangedCallback) {
   csharpEventSessionSwitch({event_handler: function (data, b) {
-    var isSessionLocked = data === 'SessionLock'
+    console.log(data);
+    var isSessionLocked = data.state === 'SessionLock'
     stateChangedCallback(isSessionLocked)
   }}, function (err, unsubscribe) {
     if (err) throw err
@@ -24,20 +25,35 @@ sessionStateDetector.stopTracking = function () {
 }
 
 var csharpEventSessionSwitch = edge.func(function () { /*
-    async (dynamic input) =>
+    using System;
+    using System.Runtime.InteropServices;
+    using System.Threading.Tasks;
+
+    class Startup 
     {
-        var eventHandler = new Microsoft.Win32.SessionSwitchEventHandler((object sender, Microsoft.Win32.SessionSwitchEventArgs e) => {
-            ((Func<object,Task<object>>)input.event_handler)(e.Reason.ToString());
-        });
+        [DllImport("user32.dll")]
+        static extern int GetSystemMetrics(int smIndex);
+  
+        public async Task<object> Invoke (dynamic input)
+        {
+            var eventHandler = new Microsoft.Win32.SessionSwitchEventHandler((object sender, Microsoft.Win32.SessionSwitchEventArgs e) => {
+                // Used to detect if the user is logged in through a remote session
+                bool isRemoteSession = GetSystemMetrics(0x1000) != 0;
+                ((Func<object,Task<object>>)input.event_handler)(new { 
+                    state = e.Reason.ToString(),
+                    isRemoteSession = isRemoteSession
+                });
+            });
 
-        Microsoft.Win32.SystemEvents.SessionSwitch += eventHandler;
+            Microsoft.Win32.SystemEvents.SessionSwitch += eventHandler;
 
-        // Return a function that can be used by Node.js to 
-        // unsubscribe from the event source.
-        return (Func<object,Task<object>>)(async (dynamic data) => {
-            Microsoft.Win32.SystemEvents.SessionSwitch -= eventHandler;
-            eventHandler = null;
-            return null;
-        });
-    };
+            // Return a function that can be used by Node.js to 
+            // unsubscribe from the event source.
+            return (Func<object,Task<object>>)(async (dynamic data) => {
+                Microsoft.Win32.SystemEvents.SessionSwitch -= eventHandler;
+                eventHandler = null;
+                return null;
+            });
+        }
+    }
 */ })
